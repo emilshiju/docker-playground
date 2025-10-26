abc0121a6677f4434d20b991fc5fa45727e500f6
verifd <settings>
2025-10-27 00:41:24 +0530

    learning

--- react-docker/src/App.tsx
+++ react-docker/src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>DOCKER Awesome</h1>
+      <h1>DOCKER AWESOME </h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
